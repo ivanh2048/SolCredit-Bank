fd3a323f00420705843017927a6ea4ab3d8411b7
ivanh2048 <ivan.hrabovskyi.uam@gmail.com>
2024-10-27 13:33:31 +0100

    .

--- bank-app/config.php
+++ bank-app/config.php
@@ -1,6 +1,6 @@
 <?php
 $host = getenv('DB_HOST') ?: 'localhost';
-$dbname = getenv('DB_NAME') ?: 'SoloCredit';
+$dbname = getenv('DB_NAME') ?: 'SolCredit';
 $username = getenv('DB_USER') ?: 'postgres';
 $password = getenv('DB_PASS') ?: '2004';
 
@@ -16,6 +16,6 @@ $mail_host = getenv('MAIL_HOST') ?: 'smtp.gmail.com';
 $mail_username = getenv('MAIL_USERNAME') ?: 'ivan.hrabovskyi.uam@gmail.com';
 $mail_password = getenv('MAIL_PASSWORD') ?: 'Obk@2022';
 $mail_port = getenv('MAIL_PORT') ?: 587; // або 465 для SSL
-$mail_from = getenv('MAIL_FROM') ?: 'solocredit@bank.com';
-$mail_from_name = getenv('MAIL_FROM_NAME') ?: 'SoloCredit';
+$mail_from = getenv('MAIL_FROM') ?: 'solcredit@bank.com';
+$mail_from_name = getenv('MAIL_FROM_NAME') ?: 'SolCredit';
 ?>
